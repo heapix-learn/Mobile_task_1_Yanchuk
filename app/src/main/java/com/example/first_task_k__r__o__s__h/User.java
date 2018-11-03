5cb89481e72e7d97976b0bf6ca88516f1c5bc8bd
Igor Yanchuk <igor201012015@gmail.com>
2018-11-03 19:57:27 +0300

    created authorization with database

---
+++ app/src/main/java/com/example/first_task_k__r__o__s__h/User.java
@@ -0,0 +1,13 @@
+package com.example.first_task_k__r__o__s__h;
+
+public class User {
+    public long userId;
+    public String username;
+    public String password;
+
+    public User(long userId, String username, String password){
+        this.userId=userId;
+        this.username=username;
+        this.password=password;
+    }
+}
