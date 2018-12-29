56ff50727996fdd00aae8d112b813d4ae11d9144
Agent00krosh <igor201012015@gmail.com>
2018-12-29 14:25:42 +0200

    Добавил авторизацию через телефон и шаблон для авторизации по почте

---
+++ app/src/main/java/com/example/first_task_k__r__o__s__h/AuxiliaryComponents/EmailAuthorization.java
@@ -0,0 +1,15 @@
+package com.example.first_task_k__r__o__s__h.AuxiliaryComponents;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.first_task_k__r__o__s__h.R;
+
+public class EmailAuthorization extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_authorization);
+    }
+}
